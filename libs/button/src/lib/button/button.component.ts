b0aea2b67ce6b9c090267a9ac14f1ea0fdae30ab
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-08-09 12:10:25 -0300

    Vídeo 3.4 - Criando variantes do tema violeta

--- libs/button/src/lib/button/button.component.ts
+++ libs/button/src/lib/button/button.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 type Variant = 'primary' | 'secondary' | 'tertiary';
+type Theme = 'blue' | 'violet';
 
 @Component({
   selector: 'ab-button',
@@ -14,8 +15,9 @@ export class ButtonComponent {
   @Input() text = '';
   @Input() variant: Variant = 'primary';
   @Input() disabled = false;
+  @Input() theme: Theme = 'blue';
 
   getClasses(): string {
-    return this.variant;
+    return `${this.variant} ${this.theme}`;
   }
 }
--- libs/storybook-host/src/stories/button.component.stories.ts
+++ libs/storybook-host/src/stories/button.component.stories.ts
@@ -13,7 +13,8 @@ export const PrimaryButton: Story = {
   args: {
     text: 'Action',
     variant: 'primary',
-    disabled: false
+    disabled: false,
+    theme: 'blue'
   }
 };
 
@@ -52,3 +53,45 @@ export const TertiaryButtonDisabled: Story = {
     disabled: true
   }
 }
+
+export const PrimaryButtonViolet: Story = {
+  args: {
+    ...PrimaryButton.args,
+    theme: 'violet'
+  }
+};
+
+export const PrimaryButtonDisabledViolet: Story = {
+  args: {
+    ...PrimaryButtonDisabled.args,
+    theme: 'violet'
+  }
+};
+
+export const SecondaryButtonViolet: Story = {
+  args: {
+    ...SecondaryButton.args,
+    theme: 'violet'
+  }
+};
+
+// export const SecondaryButtonDisabled: Story = {
+//   args: {
+//     ...SecondaryButton.args,
+//     disabled: true
+//   }
+// };
+
+// export const TertiaryButton: Story = {
+//   args: {
+//     ...PrimaryButton.args,
+//     variant: 'tertiary'
+//   }
+// }
+
+// export const TertiaryButtonDisabled: Story = {
+//   args: {
+//     ...TertiaryButton.args,
+//     disabled: true
+//   }
+// }
