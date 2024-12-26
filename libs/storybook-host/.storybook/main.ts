2925f884136ded34b6a7025bc848af289063b901
Mike Oliveira <mikeoliveira@outlook.com>
2024-12-26 14:04:24 -0300

    feat(capitulo-1): adicionando input - variaveis css

--- libs/storybook-host/.storybook/main.ts
+++ libs/storybook-host/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: ['../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/angular',
---
+++ libs/storybook-host/src/lib/Molecules/input.component.stories.ts
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { InputComponent } from '@alfabit-alura/input';
+
+const meta: Meta<InputComponent> = {
+  component: InputComponent
+};
+export default meta;
+type Story = StoryObj<InputComponent>;
+
+export const Primary: Story = {
+  args: {},
+};
