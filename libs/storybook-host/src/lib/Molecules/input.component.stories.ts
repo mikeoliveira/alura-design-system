3b449790506aa6b50056d8f507d125d92bbf66a4
Mike Oliveira <mikeoliveira@outlook.com>
2024-12-26 14:19:30 -0300

    feat(capitulo-2): input com label

--- libs/storybook-host/src/lib/Molecules/input.component.stories.ts
+++ libs/storybook-host/src/lib/Molecules/input.component.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/angular';
 import { InputComponent } from '@alfabit-alura/input';
 
 const meta: Meta<InputComponent> = {
-  component: InputComponent
+  component: InputComponent,
 };
 export default meta;
 type Story = StoryObj<InputComponent>;
@@ -11,10 +11,25 @@ export const Primary: Story = {
   args: {},
 };
 
-
 export const Multiline: Story = {
   args: {
     ...Primary.args,
-    multiline: true
+    multiline: true,
+  },
+};
+
+export const PrimaryWithLabel: Story = {
+  args: {
+    label: 'Label',
+    id: '1',
+  },
+};
+
+export const MultilineWithLabel: Story = {
+  args: {
+    ...Primary.args,
+    multiline: true,
+    label: 'Label',
+    id: '2',
   },
 };
